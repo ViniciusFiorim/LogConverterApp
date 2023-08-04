bb721d6e1618939c3241ab6b097471bc3c63424a
Vinicius Fiorim <vinicius.fiorim@outlook.com>
2023-08-04 20:20:00 -0300

    Projeto de LogConverter. Criado para objetivo de desafio solicitado para seleção de cargo de desenvolvedor para empresa Useall

---
+++ LogConverterTests/LogConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace LogConverterTests
+{
+    [TestClass]
+    public class LogConverterTests
+    {
+        [TestMethod]
+        public void TestConvert()
+        {
+            // Arrange
+            string input = "312|200|HIT|\"GET /robots.txt HTTP/1.1\"|100.2";
+            string expectedOutput = "GET 200 /robots.txt 100.2 312 HIT";
+
+            string inputPath = Path.GetTempFileName();
+            File.WriteAllText(inputPath, input);
+
+            string outputPath = Path.GetTempFileName();
+
+            try
+            {
+                // Act
+                LogConverter.Convert(inputPath, outputPath);
+
+                // Assert
+                string[] outputLines = File.ReadAllLines(outputPath);
+                Assert.AreEqual(1, outputLines.Length);
+                Assert.AreEqual(expectedOutput, outputLines[0]);
+            }
+            finally
+            {
+                File.Delete(inputPath);
+                File.Delete(outputPath);
+            }
+        }
+
+        [TestMethod]
+        public void TestGetCacheStatus()
+        {
+            // Arrange
+            string hitStatus = "HIT";
+            string missStatus = "MISS";
+            string invalidateStatus = "INVALIDATE";
+            string unknownStatus = "UNKNOWN";
+
+            // Act & Assert
+            Assert.AreEqual("HIT", LogConverter.GetCacheStatus(hitStatus));
+            Assert.AreEqual("MISS", LogConverter.GetCacheStatus(missStatus));
+            Assert.AreEqual("REFRESH_HIT", LogConverter.GetCacheStatus(invalidateStatus));
+            Assert.AreEqual("UNKNOWN", LogConverter.GetCacheStatus(unknownStatus));
+        }
+    }
+}
